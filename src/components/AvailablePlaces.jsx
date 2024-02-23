159a1d5a143c46e782d2ff7c2894ae3d16f52bd8
MehmetKaldirim <mathdenizi@gmail.com>
2024-02-23 01:32:46 +0100

    put https request work properly

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,25 +3,22 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Eror.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import {fetchAvailablePlaces} from '../http.js'
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState();
-
+  
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetched places");
-        }
+        const places = await fetchAvailablePlaces();        
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
